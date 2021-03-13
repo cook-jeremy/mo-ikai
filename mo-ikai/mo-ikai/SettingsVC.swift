3b53f56da4a88d2ac8a927148276f33063a40958
Jeremy <jeremy@monty-mac-pro.local>
2021-03-13 10:42:41 -0600

    embedded in nav controllers

---
+++ mo-ikai/mo-ikai/SettingsVC.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  mo-ikai
+//
+//  Created by Jeremy on 3/10/21.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
